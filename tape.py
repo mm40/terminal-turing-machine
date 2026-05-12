678b2da70929d9c22bbdbea8922bf275c8e88a26
mm40 <mihailomal@gmail.com>
2026-05-12 18:53:47 +0200

    Adds Tape __str__ function

--- tape.py
+++ tape.py
@@ -11,3 +11,26 @@ class Tape:
         self._headPosition = headPosition
         self._displayWidth = displayWidth
 
+
+    def __str__(self):
+        line1 = line3 = "----"
+        line2 = " ..."
+        cnt = self._displayWidth// 2
+        for i in range(self._headPosition - cnt, self._headPosition + cnt + 1):
+            field = self._data.get(i, blancCharacter)
+            width = len(field)
+            topchar = "-"
+            if cnt == 0:
+                topchar = "V"
+            if width % 2 == 0:
+                topchar = "\b" + topchar
+            line1 = line1 + "--" + width//2 * '-' + topchar + width//2 * '-' + "-"
+            line2 = line2 + "| " + field + " "
+            line3 = line3 + "--" + width * "-" + "-"
+            cnt = cnt - 1
+
+        print(line1 + "+----")
+        print(line2 + "|...")
+        print(line3 + "+----")
+        return 'HEAD pos = ' + str(self._headPosition) + '| DATA = ' + str(self._data)
+
